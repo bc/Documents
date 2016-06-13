ec57d8728e33912be008e3d276b29686f0ff8f5f
dhagen.work <dhagen@usc.edu>
2016-06-13 16:55:38 -0700

    Playing around on the playground to find the magnitude of my basketball initial velocity.

--- PythonPlayground.py
+++ PythonPlayground.py
@@ -19,7 +19,7 @@ plt.show()
 person = raw_input("Enter your name: ")
 string = "Hello " + person
 print string
-"""
+
 x = np.arange(0,3*3.14,0.01)
 y = np.sin(x) + np.sin(2*x)
 xtick = np.arange(0,3*3.14+0.01,1.57)
@@ -31,3 +31,27 @@ plt.xticks(xtick,xticklabel,fontsize = 16)
 plt.setp(plt.xticks()[1])
 plt.title(r'$\Gamma(\theta) = \mathrm{sin}(\theta) + \mathrm{sin}(2\theta)$', fontsize = 20)
 plt.show()
+"""
+
+HeightInches = 71
+DegreesToRadianFactor = 3.14159/180
+ReleaseAngle = 30*DegreesToRadianFactor
+Angle1Final = 120*DegreesToRadianFactor
+Angle2Final = 84*DegreesToRadianFactor
+Angle3Final = -35*DegreesToRadianFactor
+Height = 2.54*HeightInches
+ShoulderToElbowHeight = 0.186*Height
+ForearmLength = 0.146*Height
+HandLength = 0.108*Height
+FinalPositionInX = ShoulderToElbowHeight*np.sin(Angle1Final) \
+		+ ForearmLength*np.sin(Angle1Final + Angle2Final) \
+		+ HandLength*np.sin(Angle1Final + Angle2Final - Angle3Final)
+print "Final Position in X: " + str(FinalPositionInX)
+FinalPositionInY = -ShoulderToElbowHeight*np.cos(Angle1Final) \
+		- ForearmLength*np.cos(Angle1Final + Angle2Final) \
+		- HandLength*np.cos(Angle1Final + Angle2Final - Angle3Final)
+print "Final Position in Y: " + str(FinalPositionInY)
+InitialProjectileVelocity = np.sqrt(-490*((434.3+0.152*Height-FinalPositionInX-11.9*np.cos(ReleaseAngle))**2) \
+							/((((np.cos(ReleaseAngle))**2)*(304.8-0.87*Height-FinalPositionInY)) \
+							-(np.sin(ReleaseAngle)*np.cos(ReleaseAngle)*(434.3+0.152*Height-FinalPositionInX))))
+print "Final Velocity is " + str(InitialProjectileVelocity/100) + " m/s"
