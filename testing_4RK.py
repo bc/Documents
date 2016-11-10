c4c3138ff2d061dc373e47a085e3b22616862073
dhagen.work <dhagen@usc.edu>
2016-11-10 10:55:53 -0800

    changing plotting options to downsample... too much memory otherwise.

--- testing_4RK.py
+++ testing_4RK.py
@@ -1,8 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt 
-from amm_functions import statusbar
+from amm_functions import statusbar,append_dictionary
 import time
 import pickle
+
+
+def fourth_order_runge_kutta(f,g,t,x,y,dt):
+	def k_and_l_values(f,g,t,x,y,dt):
+		l0, k0 = f(t,x,y)*dt, 					g(t,x,y)*dt
+		l1, k1 = f(t+dt/2,x+l0/2,y+k0/2)*dt, 	g(t+dt/2,x+l0/2,y+k0/2)*dt
+		l2, k2 = f(t+dt/2,x+l1/2,y+k1/2)*dt, 	g(t+dt/2,x+l1/2,y+k1/2)*dt
+		l3, k3 = f(t+dt,x+l2,y+k2)*dt, 			g(t+dt,x+l2,y+k2)*dt
+		return([l0,l1,l2,l3],[k0,k1,k2,k3])
+	l,k = k_and_l_values(f,g,t,x,y,dt)
+	next_x = x + (l[0] + 2*(l[1] + l[2]) + l[3])/6
+	next_y = y + (k[0] + 2*(k[1] + k[2]) + k[3])/6
+	return(next_x,next_y)
 
 def freq_d(t):
 	p = 2
@@ -23,55 +36,51 @@ def gamma(t):
 def C(LengthFirstDeriv):
 	result = 0.58*(LengthFirstDeriv >= 0) + 0.42 # when true C = 1 else, C = 0.42  
 	return(result)
-def f(t,y,T,Length,LengthFirstDeriv,LengthSecondDeriv):
-	R = 0.46 #length dependency of the force-velocity relationship
-	a = 0.3
-	K_SR = 10.4649
-	K_PR = 0.15
-	M = 0.0002 # intrafusal fiber mass
+def define_f(Length,LengthFirstDeriv,LengthSecondDeriv):
+	def f(t,y,T):
+		R = 0.46 #length dependency of the force-velocity relationship
+		a = 0.3
+		K_SR = 10.4649
+		K_PR = 0.15
+		M = 0.0002 # intrafusal fiber mass
 
-	LN_SR = 0.0423
+		LN_SR = 0.0423
 
-	L0_SR = 0.04 #in units of L0
-	L0_PR = 0.76 #polar region rest length
+		L0_SR = 0.04 #in units of L0
+		L0_PR = 0.76 #polar region rest length
 
-	result = float((K_SR/M)*((C(LengthFirstDeriv) * beta(t) * np.sign(LengthFirstDeriv-y/K_SR) \
-								* ((abs(LengthFirstDeriv-y/K_SR))**a) * (Length - L0_SR - T/K_SR - R)) \
-								+ K_PR * (Length - L0_SR - T/K_SR - L0_PR) \
-								+ M*LengthSecondDeriv + gamma(t) - T))
-	return(result)
+		result = float((K_SR/M)*((C(LengthFirstDeriv) * beta(t) * np.sign(LengthFirstDeriv-y/K_SR) \
+									* ((abs(LengthFirstDeriv-y/K_SR))**a) * (Length - L0_SR - T/K_SR - R)) \
+									+ K_PR * (Length - L0_SR - T/K_SR - L0_PR) \
+									+ M*LengthSecondDeriv + gamma(t) - T))
+		return(result)
+	return(f)
+
 def g(t,y,T):
 	result = y
 	return(result)
-def k_and_l_values(t,y,T,Length,LengthFirstDeriv,LengthSecondDeriv,dt):
-	k0 = g(t,y[-1],T[-1])*dt
-	l0 = f(t,y[-1],T[-1],Length,LengthFirstDeriv,LengthSecondDeriv)*dt
-	k1 = g(t+dt/2,y[-1]+l0/2,T[-1]+k0/2)*dt
-	l1 = f(t+dt/2,y[-1]+l0/2,T[-1]+k0/2,Length,LengthFirstDeriv,LengthSecondDeriv)*dt
-	k2 = g(t+dt/2,y[-1]+l1/2,T[-1]+k1/2)*dt
-	l2 = f(t+dt/2,y[-1]+l1/2,T[-1]+k1/2,Length,LengthFirstDeriv,LengthSecondDeriv)*dt
-	k3 = g(t+dt,y[-1]+l2,T[-1]+k2)*dt
-	l3 = f(t+dt,y[-1]+l2,T[-1]+k2,Length,LengthFirstDeriv,LengthSecondDeriv)*dt
-	return([k0,k1,k2,k3],[l0,l1,l2,l3])
-def fourth_order_runge_kutta(t,y,T,Length,LengthFirstDeriv,LengthSecondDeriv,dt):
-	"""
-	t must be a scalar of value at previous timestep. y and T must be lists with 
-	initial values (i.e. most recent values at index [-1])
-	"""
-	k,l = k_and_l_values(t,y,T,Length,LengthFirstDeriv,LengthSecondDeriv,dt)
-	y.append(y[-1] + (l[0] + 2*(l[1] + l[2]) + l[3])/6)
-	T.append(T[-1] + (k[0] + 2*(k[1] + k[2]) + k[3])/6)
+def k_and_l_values(f,g,t,x,y,dt):
+	l0, k0 = f(t,x,y)*dt, 					g(t,x,y)*dt
+	l1, k1 = f(t+dt/2,x+l0/2,y+k0/2)*dt, 	g(t+dt/2,x+l0/2,y+k0/2)*dt
+	l2, k2 = f(t+dt/2,x+l1/2,y+k1/2)*dt, 	g(t+dt/2,x+l1/2,y+k1/2)*dt
+	l3, k3 = f(t+dt,x+l2,y+k2)*dt, 			g(t+dt,x+l2,y+k2)*dt
+	return([l0,l1,l2,l3],[k0,k1,k2,k3])
+def fourth_order_runge_kutta(f,g,t,x,y,dt):
+	l,k = k_and_l_values(f,g,t,x,y,dt)
+	next_x = x + (l[0] + 2*(l[1] + l[2]) + l[3])/6
+	next_y = y + (k[0] + 2*(k[1] + k[2]) + k[3])/6
+	return(next_x,next_y)
 
-y = [0]
-T = [0]
-dt = (1e-6)*5/3
+Bag1_RK = {'TensionFirstDeriv':[0],'Tension':[0]}
+dt = 1e-5*(3/5)
 t = np.arange(0,15+dt,dt)
 Length = 1.0766863061118987
 LengthFirstDeriv = 0
 LengthSecondDeriv = 0
+f = define_f(Length,LengthFirstDeriv,LengthSecondDeriv)
 StartTime = time.time()
 for i in range(len(t)):
-	fourth_order_runge_kutta(t[i],y,T,Length,LengthFirstDeriv,LengthSecondDeriv,dt)
+	append_dictionary(Bag1_RK,['TensionFirstDeriv','Tension'],fourth_order_runge_kutta(f,g,t[i],Bag1_RK['TensionFirstDeriv'][-1],Bag1_RK['Tension'][-1],dt))
 	statusbar(i,len(t),StartTime=StartTime,Title='4th Order Runge Kutta')
 
 print('\n')
@@ -125,16 +134,16 @@ def bag1_model(CE,Bag1,SamplingPeriod):
 	Bag1['DynamicSpindleFrequency'].append(DynamicSpindleFrequency)
 
 CE = {'Length':[Length],'Velocity':[0],'Acceleration':[0]}
-Bag1 = {'GammaDynamicGain':50,'DynamicSpindleFrequency':[0],'Tension':[0],'TensionFirstDeriv':[0]}
-SamplingPeriod = 1e-6
+Bag1_FE = {'GammaDynamicGain':50,'DynamicSpindleFrequency':[0],'Tension':[0],'TensionFirstDeriv':[0]}
+SamplingPeriod = 1e-7
 Time = np.arange(0,15+SamplingPeriod,SamplingPeriod)
 StartTime = time.time()
 for i in range(len(Time)):
-	bag1_model(CE,Bag1,SamplingPeriod)
+	bag1_model(CE,Bag1_FE,SamplingPeriod)
 	statusbar(i,len(Time),StartTime=StartTime,Title='Forward Euler')
-Bag1Tension = Bag1['Tension'][:-1]
+Bag1Tension = Bag1_FE['Tension'][:-1]
 # Time,Bag1Tension = pickle.load(open('Bag1Tension_FE.pkl','rb'))
 
-plt.plot(Time,Bag1Tension,'r--')
-plt.plot(t,T[:-1],'b')
-plt.show()
+# plt.plot(Time[range(0,len(Time),100)],Bag1Tension[range(0,len(Time),100)],'r--')
+# plt.plot(t[range(0,len(t),10)],Bag1_RK['Tension'][:-1][range(0,len(t),10)],'b')
+# plt.show()
